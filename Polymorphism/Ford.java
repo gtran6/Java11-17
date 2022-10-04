f8e3f3a307e6b66dec16339dd9bab19b97f7fe6a
gtran6 <gtran6@depaul.edu>
2022-10-04 15:41:24 -0500

    7th commit

---
+++ Polymorphism/Ford.java
@@ -0,0 +1,22 @@
+package Polymorphism;
+
+class Ford extends Car {
+    public Ford(int cylinders, String name) {
+        super( cylinders, name );
+    }
+
+    @Override
+    public void startEngine() {
+        super.startEngine();
+    }
+
+    @Override
+    public void accelerate() {
+        super.accelerate();
+    }
+
+    @Override
+    public void brake() {
+        super.brake();
+    }
+}
---
+++ Polymorphism/Holden.java
@@ -0,0 +1,22 @@
+package Polymorphism;
+
+class Holden extends Car {
+    public Holden(int cylinders, String name) {
+        super( cylinders, name );
+    }
+
+    @Override
+    public void startEngine() {
+        super.startEngine();
+    }
+
+    @Override
+    public void accelerate() {
+        super.accelerate();
+    }
+
+    @Override
+    public void brake() {
+        super.brake();
+    }
+}
